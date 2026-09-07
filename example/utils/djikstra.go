7e3593816a5264537880ae4306bf807b60de2678
LabKatCo <labkatcorp@gmail.com>
2026-09-06 23:05:05 -0700

    djikstra example work

--- example/utils/djikstra.go
+++ example/utils/djikstra.go
@@ -1,7 +1,57 @@
 package utils
 
+import (
+	"fmt"
+	"strings"
+)
+
 //bona:pure
 func ShortestPath(n int, edges [][]int, src int) map[int]int {
-	shortestPaths := map[int]int{}
+
+	pathsToWeights := map[string]int{}
+
+	// for all edges, map start-end path to weight
+	for _, edge := range edges {
+		start := edge[0]
+		end := edge[1]
+		weight := edge[2]
+
+		pathsToWeights[fmt.Sprintf("%v-%v", start, end)] = weight
+	}
+
+	for range n {
+		for path, weight := range pathsToWeights {
+			pathStart := string(path[0])
+			pathEnd := string(path[len(path)-1])
+
+			for possibleNextPath, nextWeight := range pathsToWeights {
+				if strings.HasPrefix(possibleNextPath, pathEnd) && !strings.HasSuffix(possibleNextPath, pathStart) {
+					fullPath := path + "-" + string(possibleNextPath[len(possibleNextPath)-1])
+					pathsToWeights[fullPath] = weight + nextWeight
+				}
+			}
+		}
+	}
+
+	shortestPaths := map[int]int{src: 0}
+
+	srcIndex := fmt.Sprintf("%v", src)
+
+	for i := range n {
+		nIndex := fmt.Sprintf("%v", i)
+
+		for fullPath, weight := range pathsToWeights {
+			if strings.HasPrefix(fullPath, srcIndex) && strings.HasSuffix(fullPath, nIndex) {
+				currentShortest, found := shortestPaths[i]
+
+				if !found {
+					shortestPaths[i] = weight
+				} else if weight < currentShortest {
+					shortestPaths[i] = weight
+				}
+			}
+		}
+	}
+
 	return shortestPaths
 }
--- example/utils/djikstra_test.go
+++ example/utils/djikstra_test.go
@@ -6,7 +6,6 @@ import (
 	utils2 "github.com/labkatco/bona/example/__bona_mirror/utils"
 )
 
-// {{0:0}, {1:7}, {2:3}, {3:9}, {4:5}}
 func TestShortestPath(t *testing.T) {
 	CheckDeepEqual(t,
 		utils2.ShortestPath(
@@ -23,4 +22,18 @@ func TestShortestPath(t *testing.T) {
 		),
 		map[int]int{0: 0, 1: 7, 2: 3, 3: 9, 4: 5},
 	)
+
+	// CheckDeepEqual(t,
+	// 	utils2.ShortestPath(
+	// 		5,
+	// 		[][]int{
+	// 			{0, 1, 10},
+	// 			{0, 2, 3},
+	// 			{1, 3, 2},
+	// 			{2, 1, 4},
+	// 			{2, 3, 8}},
+	// 		0,
+	// 	),
+	// 	map[int]int{0: 0, 1: 7, 2: 3, 3: 9, 4: 5},
+	// )
 }
